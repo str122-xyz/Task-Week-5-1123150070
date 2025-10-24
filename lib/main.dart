3e2ffb3b30b138953c6167eac737c51de09f7f06
Satriahrl_ <satriaherlambang22@gmail.com>
2025-10-24 11:41:28 +0700

    buat app bar warna indigo

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Ini Title Application',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+        backgroundColor: Colors.indigo,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: const Column(children: []),
+        ),
+      ),
     );
   }
 }
